058d9d147e7d82cb0acd5438d222037d5f4f50d1
Kacper Mejsner <kacpermejs@gmail.com>
2024-06-29 20:16:31 +0200

    Refactored

--- GameSaveSynchronizerConsole/Program.cs
+++ GameSaveSynchronizerConsole/Program.cs
@@ -1,12 +1,57 @@
 ﻿using GameSaveSynchronizerCore;
+using GameSaveSynchronizerCore.Models;
+using Google.Apis.Drive.v3;
 
 namespace GameSaveSynchronizerConsole {
     internal class Program {
 
-        public static Synchronizer synchronizer = new Synchronizer();
-
         static void Main(string[] args) {
-            synchronizer.Initialize();
+            Synchronizer.Initialize();
+
+            if (!Synchronizer.LoadWorkingFolderFromJSON()) {
+                ListAndSelectFolder();
+            }
         }
+
+        private static void ListAndSelectFolder() {
+            var folders = Synchronizer.ListDriveFiles();
+
+            Console.WriteLine("Folders:");
+            if (folders != null && folders.Count > 0) {
+                int i = 0;
+                foreach (var folder in folders) {
+                    Console.WriteLine("{0} ({1})\n", folder.Name, i++);
+                }
+
+                FolderData? selectedFolder = null;
+
+                while (selectedFolder == null) {
+
+                    // Prompt user to select a folder by entering the folder ID
+                    Console.WriteLine("Enter the ID of the folder to use: ");
+                    try {
+                        string? userInput = Console.ReadLine() ?? "-1";
+                        int folderIndex = int.Parse(userInput);
+                        var folder = folders.ElementAt(folderIndex);
+
+                        selectedFolder = new FolderData {
+                            FolderId   = folder.Id,
+                            FolderName = folder.Name
+                        };
+
+                        Synchronizer.SaveSelectedFolder(selectedFolder);
+                    }
+                    catch {
+                        Console.WriteLine("Wrong index selected! Try again");
+                        continue;
+                    }
+
+                }
+            } else {
+                Console.WriteLine("No folders found.");
+            }
+        }
+
+        
     }
 }
--- GameSaveSynchronizerCore/Synchronizer.cs
+++ GameSaveSynchronizerCore/Synchronizer.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text.Json;
 
 namespace GameSaveSynchronizerCore {
-    public class Synchronizer {
+    public static class Synchronizer {
 
         private static string[] Scopes = {
             DriveService.Scope.DriveFile,
@@ -16,52 +16,19 @@ namespace GameSaveSynchronizerCore {
 
         private static string ApplicationName = "Game Save Synchronizer";
 
-        private static DriveService _service;
+        private static DriveService? _service;
         private static FolderData? _workingFolder = null;
-        private static string _dataPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data");
-        private static string _selectedFolderFileName = "selected_folder.json";
+        private static readonly string _dataPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data");
+        private static readonly string _selectedFolderFileName = "selected_folder.json";
 
-        public async void Initialize() {
-            UserCredential credential;
-
-            string secretsPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "secrets");
-            if (!Directory.Exists(secretsPath)) {
-                Directory.CreateDirectory(secretsPath);
-            }
-            if (!Directory.Exists(_dataPath)) {
-                Directory.CreateDirectory(_dataPath);
-            }
-
-            string credPath = Path.Combine(secretsPath, "credentials.json");
-            string tokenPath = Path.Combine(secretsPath, "token");
-            var tokenStore = new FileDataStore(tokenPath, true);
-
-            using (var stream = new FileStream(credPath, FileMode.Open, FileAccess.Read)) {
-
-                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
-                    GoogleClientSecrets.Load(stream).Secrets,
-                    Scopes,
-                    "user",
-                    CancellationToken.None,
-                    tokenStore).Result;
-
-                Console.WriteLine("Credential file saved to: " + tokenPath);
-            }
-
-            // Create Drive API service.
-            _service = new DriveService(new BaseClientService.Initializer() {
-                HttpClientInitializer = credential,
-                ApplicationName = ApplicationName,
-            });
-
-
-            if (!LoadWorkingFolderFromJSON()) {
-                ListAndSelectFolder();
-            }
+        public static void Initialize() {
+            AuthorizeService();
         }
 
-        static void ListAndSelectFolder() {
-            // Define parameters of request
+        public static IList<Google.Apis.Drive.v3.Data.File> ListDriveFiles() {
+            if (_service == null) {
+                throw new InvalidOperationException("Synchronizer is not initialized properly!");
+            }
             FilesResource.ListRequest listRequest = _service.Files.List();
             listRequest.PageSize = 10;
             listRequest.Fields = "nextPageToken, files(id, name)";
@@ -69,40 +36,10 @@ namespace GameSaveSynchronizerCore {
 
             // List folders
             var folders = listRequest.Execute().Files;
-            Console.WriteLine("Folders:");
-            if (folders != null && folders.Count > 0) {
-                int i = 0;
-                foreach (var folder in folders) {
-                    Console.WriteLine("{0} ({1})\n", folder.Name, i++);
-                }
-                while (_workingFolder == null) {
-
-                    // Prompt user to select a folder by entering the folder ID
-                    Console.WriteLine("Enter the ID of the folder to use: ");
-                    try {
-                        string? userInput = Console.ReadLine() ?? "-1";
-                        int folderIndex = int.Parse(userInput);
-                        var folder = folders.ElementAt(folderIndex);
-
-                        var folderData = new FolderData {
-                            FolderId = folder.Id,
-                            FolderName = folder.Name
-                        };
-
-                        SaveSelectedFolder(folderData);
-                    }
-                    catch {
-                        Console.WriteLine("Wrong index selected! Try again");
-                        continue;
-                    }
-
-                }   
-            } else {
-                Console.WriteLine("No folders found.");
-            }
+            return folders;
         }
 
-        private static void SaveSelectedFolder(FolderData folder) {
+        public static void SaveSelectedFolder(FolderData folder) {
             _workingFolder = folder;
             string savePath = Path.Combine(_dataPath, _selectedFolderFileName);
             string json = JsonSerializer.Serialize(folder);
@@ -110,7 +47,7 @@ namespace GameSaveSynchronizerCore {
             Console.WriteLine("Selected folder ID saved to " + savePath);
         }
 
-        private static bool LoadWorkingFolderFromJSON() {
+        public static bool LoadWorkingFolderFromJSON() {
             string selectedFolderPath = Path.Combine(_dataPath, _selectedFolderFileName);
 
             if (!File.Exists(selectedFolderPath)) {
@@ -124,19 +61,51 @@ namespace GameSaveSynchronizerCore {
 
                 // Deserialize the JSON to a FolderData object
                 _workingFolder = JsonSerializer.Deserialize<FolderData>(json);
-                Console.WriteLine("Loaded working folder data: {0} ({1})", _workingFolder.FolderName, _workingFolder.FolderId);
             }
             catch (Exception ex) {
                 Console.WriteLine($"Error loading folder data: {ex.Message}");
                 return false;
             }
 
+            if (_workingFolder == null) {
+                return false;
+            }
+
+            Console.WriteLine("Loaded working folder data: {0} ({1})", _workingFolder.FolderName, _workingFolder.FolderId);
             return true;
         }
 
+        private static void AuthorizeService() {
+            string secretsPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "secrets");
+            string credPath = Path.Combine(secretsPath, "credentials.json");
+            string tokenPath = Path.Combine(secretsPath, "token");
+            var tokenStore = new FileDataStore(tokenPath, true);
 
-        public void Synchronize() {
+            createDirectories(secretsPath);
 
+            UserCredential credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                GoogleClientSecrets.FromFile(credPath).Secrets,
+                Scopes,
+                "user",
+                CancellationToken.None,
+                tokenStore).Result;
+
+            Console.WriteLine("Credential file saved to: " + tokenPath);
+
+            // Create Drive API service.
+            _service = new DriveService(new BaseClientService.Initializer() {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+        }
+
+        private static void createDirectories(string secretsPath) {
+            if (!Directory.Exists(secretsPath)) {
+                Directory.CreateDirectory(secretsPath);
+            }
+            if (!Directory.Exists(_dataPath)) {
+                Directory.CreateDirectory(_dataPath);
+            }
         }
     }
 }
